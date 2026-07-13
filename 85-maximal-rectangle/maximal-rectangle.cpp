4a7caf7aaa32375f24a0545a9e3450c15b7e0ac6
Akshat Chitransh <akshat.chitransh3108@gmail.com>
2026-07-13 11:11:19 +0530

    Time: 7 ms (66.66%) | Memory: 19 MB (83.30%) - LeetSync

---
+++ 85-maximal-rectangle/maximal-rectangle.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        int n = heights.size();
+        stack<int> st;
+        int ans = 0;
+
+        for (int i = 0; i <= n; i++) {
+            while (!st.empty() && (i == n || heights[st.top()] >= heights[i])) {
+                int h = heights[st.top()];
+                st.pop();
+                int left = st.empty() ? -1 : st.top();
+                int width = i - left - 1;
+                ans = max(ans, h * width);
+            }
+            st.push(i);
+        }
+
+        return ans;
+    }
+
+    int maximalRectangle(vector<vector<char>>& matrix) {
+        if (matrix.empty()) return 0;
+
+        int rows = matrix.size();
+        int cols = matrix[0].size();
+        vector<int> heights(cols, 0);
+        int ans = 0;
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (matrix[i][j] == '1')
+                    heights[j]++;
+                else
+                    heights[j] = 0;
+            }
+
+            ans = max(ans, largestRectangleArea(heights));
+        }
+
+        return ans;
+    }
+};
